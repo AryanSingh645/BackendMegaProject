c7662d3a3c513909250f36612f70c75a7e03bdd7
AryanSingh645 <singhjee30476@gmail.com>
2024-06-23 21:44:02 +0530

    Add login, logout and token refresh routes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -60,7 +60,7 @@ userSchema.pre("save", async function(next){
 })
 
 userSchema.methods.isPasswordCorrect = async function(password){
-    await bcrypt.compare(password, this.password);
+    return await bcrypt.compare(password, this.password);
 }
 
 userSchema.methods.generateAccessToken = function(){
@@ -74,6 +74,18 @@ userSchema.methods.generateAccessToken = function(){
         process.env.ACCESS_TOKEN_SECRET,
         {
             expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
         }
     )
 }
